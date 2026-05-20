2814f2bbceab88a4f0999a6d0a76f6b238db43a5
Cameron Fox <thegamingfoxprogram@gmail.com>
2026-05-19 22:15:00 -0400

    Begin Adding the Patrol State System.

---
+++ Assets/Scripts/StateMachines/Enemy/EnemyPatrolState.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class EnemyPatrolState : EnemyBaseState
+{
+    public EnemyPatrolState(EnemyStateMachine stateMachine) : base(stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Exit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
