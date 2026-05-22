52379d5156b97662ce2fb000b07890696c9dd392
Pasindu Siriwardana <dragonpasindu@gmail.com>
2026-05-22 23:57:53 +0900

    Bug Fix for holding one type of item 1 at a time

--- Assets/Scripts/Inventory/PickupItem.cs
+++ Assets/Scripts/Inventory/PickupItem.cs
@@ -11,6 +11,8 @@ namespace ThirdPerson.Inventory
         {
             if (other.TryGetComponent(out Inventory inventory))
             {
+                if (inventory.HasItem(Item.ItemType)) { return; }// check if we already have the item, if so we can skip the pickup
+
                 Item.transform.SetParent(inventory.transform); // detach before destroy
                 if (inventory.AddItem(Item))
                 {
