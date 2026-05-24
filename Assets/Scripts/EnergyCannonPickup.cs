815e56c86b980048f4b9816bc8ad39fdfd08225a
robinjamesmusic <rcrinson@hotmail.com>
2026-05-24 08:09:06 +0100

    MusicTest, SFXTest

--- Assets/Scripts/EnergyCannonPickup.cs
+++ Assets/Scripts/EnergyCannonPickup.cs
@@ -5,9 +5,13 @@ public class EnergyCannonPickup : MonoBehaviour
     public Player player;
     public Transform cannon;
 
+    public AudioSource pickupAudio;
+
     private void OnTriggerEnter(Collider other)
     {
         player.AttachCannon(cannon);
+
+        pickupAudio.Play();
 
         gameObject.SetActive(false);
     }
